a013fd3c458d883c8ff671149dc071f013713530
Anil Maurya <anil2711@hotmail.com>
2024-10-10 16:37:05 +0530

    CQRS,BuildingBlock,Model,Product and Postgresdb and Docker Compose

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,47 @@
+﻿
+namespace Catalog.API.Products.CreateProduct
+{
+    /*
+      In ASP.NET Core, a record is a feature introduced in C# 9.01
+      .Records are a reference type, similar to classes, but they have value semantics, meaning they are immutable and can be compared by value2
+  .   This makes them particularly useful for modeling data3
+  .
+      public record Person(string FirstName, string LastName);
+      In this example, the Person record has two properties: FirstName and LastName4
+  .   Records are great for creating data transfer objects (DTOs) and for ensuring that your data models are immutable and easily comparable5
+  .
+      */
+
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+
+    internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            //  Create Product entity from Command Object
+            //  Save the database
+            //  Return Create ProductResult result
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+
+
+            };
+
+            // Save to Database
+
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+             //return result
+
+            return new CreateProductResult(product.Id);
+        }
+    }
+}
