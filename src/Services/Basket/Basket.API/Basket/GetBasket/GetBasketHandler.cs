d517be439548c19ca3e7a9d9d05b8d3988fca9d0
Anil Maurya <anil2711@hotmail.com>
2024-11-17 01:58:26 +0530

    GetBasket Handler added

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace Basket.API.Basket.GetBasket
+{
+    public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+
+    public record GetBasketResult(ShoppingCart Item);
+    public class  GetBaskeQuerytHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
+    {
+        public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+        {
+            //To do : get basket from Database
+            //var basket=await _repositoyry.GetBasket(request.UserName)
+            return new GetBasketResult(new ShoppingCart("swn"));
+        }
+    }
+}
